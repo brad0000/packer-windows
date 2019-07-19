c53d73293dab81a7bdc3ed95f6ae8791cd166f78
Brad Bishop <brad0000@bmbit.com>
2019-07-19 15:40:21 +1200

    move rebuild script out of repo

--- rebuild-win10.ps1
+++
@@ -1,4 +0,0 @@
-set-location d:\devops\brad0000\packer-windows
-packer build -on-error=abort .\windows_10.json
-$timeStamp = [System.DateTime]::Now.ToString("yyyy-MM-ddTHH.mm.ss")
-move-item .\windows_10_virtualbox.box "D:\Repos\alchemy\boxes\windows_10_virtualbox_$timeStamp.box"
